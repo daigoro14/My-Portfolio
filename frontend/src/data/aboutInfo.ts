f433bdc468791a18a4a632fccc3da9b1e261122d
daigoro <daigoropersson@gmail.com>
2023-06-03 20:53:46 +0200

    about images added

--- frontend/src/data/aboutInfo.ts
+++ frontend/src/data/aboutInfo.ts
@@ -1,3 +1,6 @@
+import graduation from '../photos/Me/graduation.jpg'
+import skate from '../photos/Me/skate.jpg'
+
 const about = [
     {
         title: "Studies",
@@ -7,7 +10,7 @@ const about = [
 
         Through engaging examples, clear explanations, and hands-on exercises, I gained a solid understanding of the fundamentals required to work as a developer and developed the mindset of a developer. I've done a lot of studying on my own as well, plus been fortunate to have more experienced developers in my presence to guide and mentor me.` ,
         alt: "graduation",
-        img: "https://lh3.googleusercontent.com/pw/AJFCJaWj9zhf7HnQm8fNUsz2KkSxLx0Cy0YzEXRfF5Pk85s7RXzsEhWiMqPc4-zVtjo10o9dIbzgz_Wi4x-pPmqE1iLLzuhJ7ago4nxu3K9QpRjXkguTOchGJQajPjb-NQTZh76jQG5_NeUjo0AXGnA3qFPTxZ4lVR5wrpTb4XDTo4yOrWuMl13niyt_M2Wu8WrZTSoPfPc4ZDfvnJNOePygTxNMfyL1aJkHychyH3BBImVvzh703uVyZSLhvUBxSq3WdQdoY28PoYcDG01y13Hj7Qujp3W7OKD4FsmoXR35_2QT1ffx5q_l_s9Dvv6oQY1tL2_5MNik1S75YRjt6tNp8WmegGdTFpM46pzMH3vvoCxRgz3N9RDmRnxM7ygo1MhXuRGWRjOpU_vYcwLKf9Kh4tqUW_eltL4zV6kWnNkJ3hQw9oCuiYEeueBmeZ4Gh6xSd4Zt6LjgHQ4l3DFd83BbLImMRRquyWG1cBc1ni05_kKUrzXYxupJZ3Jsn3BtSXeSU1brpjS3EXcCu-4doQw3WE-Ltb5u7LXj4uRNRC0rtqCCFJ67s4L_ZhbAy5P6a0MVXwwK-QM_n3KRvyaVLlV5cGc5dOTsBij2h7m_LgFxr_Wi21j7n-kFmTMugrHoDP52YKqyZ26AGXyVK4yM8cxV_poKmaA83Hw82CC1QcmiQ5gs98ujXp4Ps_WMzdxk9fWJtojz17Z0gJFzWGvtutzO5LN5UxwSABRLdWze1dQarmjE2vGgSbfrQGsQIhS1-OJSV_KnJnAd4X6TMNfupOHuwO9yqDZQvrksBC8XRnMVV1d43yX8XJJKL22hIVJsF_BgqKx52dzHcIMGQMv3LnZicGqSz2NfsrzLJVGtCcJ5mXgWS7mZ15_Wpsa9taenspxoaF_Bf3uM1dKpK7UJwN9vBscUZxXfJ2-IJ6wJjB10VQJQE3ULtFqVrag6qGsW=w1390-h1778-s-no?authuser=0"
+        img: graduation
     },
     {
         title: "Hobbies",
@@ -17,7 +20,7 @@ const about = [
 
         Each one of my hobbies has taught me valuable lessons that have come in handy in different occasions throughtout my life. Beside my hobbies I also enjoy spending time with family and friends.`,
         alt: "skate",
-        img: "https://lh3.googleusercontent.com/pw/AJFCJaVpgfVf1tblK82Bd4bI0d1Jc4C3Y4_EK5jO3dIBKG-uix97uv5uywzo86zw2w2cdSCTyJ1hFpYV7hgzmSOBwwG5UMd4hXsNsf0AOlERkxoes1SoBT7LLssEDJW06LiULfOs_j6GHSuF5Ko78gMPfMGwrWjHC4yABOZAAh77qcxLKzTpWbQLUMG1H1HEG_RNkH5nWgnserTd4JdpByAT9SNdqfjYuJajaAB_3b3oNdX3T5JotOCwqijwT5NnHRv6Rf0_6K87XPcoAlyhsFseHFNVq0VxOzYCoGo6IT7koMSGJxyFOytk6LU8ANkgjJ17Q4OA165pOHFBKI84lgn0SREPXzJehbnQ-5_BFr05NR-in3_4Wn_ivSUw5gjfVD7sO6VMv_-zokrhXgrJkFphBjD7g6giVxzAKffgZNKIkHC6RhX1gQfXewj1KbLf9cNCMqKbMFMfCX0hKYBbbR8mipYL0K8gvpULrkVU2-RsnyaHgwYBbFE9B4Z9_g8ADkMHDyBX7mYYkW_a3ph7nzPve2TLCAozWU75VzIY15ERYtpuiKQrTvO2Aa3HwNf55RGsUKh92_dInG6LZO2OwiZ8wwY8_5Ficr0RnmlsBPWLhAP8qSRaFCeX84v0aQr_2jk32QXlfyFzXM44WhatDYJ6Zxe_xx4_loJ3g8JJfg2_TLnrbuCoKygr9rJmzZYnVl31utHDl2EdqBVuoh_yEEvNgV7RTlaSOAvLDUUdd4Bwl8iGN2jBp2S60jZdeOxs25yRXM7UK1QPU7foH7xSWk3-tzU7gWDusClPqnTu-obiaxM-FDmyoooP4CRDw3QHcHyjZaZMvDXDkcsZmQpqA68mslmEFAhLESLbBQa_3JMqpaPgH9G2klkjMrN4prS2o0Yf8Eek7XMKrWFag1lv-Ps-NaPlukVravgiFc2ErOWnM3nmqSSTxkzIJqtBij4I=w1367-h1367-s-no?authuser=0"
+        img: skate
     }
 ]
 
